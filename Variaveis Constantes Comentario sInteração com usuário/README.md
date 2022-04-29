7928070f2686b69eaade4e0d7d30d69e89c76c54
CORP\roliota <rubem.oliota@capgemini.com>
2022-04-29 18:29:27 -0300

    subindo para nao deixar aqui

---
+++ Variaveis Constantes Comentario sInteração com usuário/README.md
@@ -0,0 +1,14 @@
+# Aprendendo sobre variaveis 
+ 
+* Variaveis
+    * Tipos
+    * Nomenclatura
+* Constantes
+    * Sintaxe
+* Comentários
+    * Unitário
+    * Multiplos
+    * Atalhos
+* Interação com o usuário
+    * Console
+    * JOptionPane
---
+++ Variaveis Constantes Comentario sInteração com usuário/src/br/com/entra21/logicajava/variaveis/Main.java
@@ -0,0 +1,15 @@
+package br.com.entra21.logicajava.variaveis;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Prática sobre variaveis");
+		
+		short ano=2022;
+		
+		ano/=2;
+		System.out.println(ano);
+
+	}
+
+}
